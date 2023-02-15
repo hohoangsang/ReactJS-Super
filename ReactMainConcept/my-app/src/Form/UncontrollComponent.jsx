5ded25cdc64c22ebb73f50a18feca07503607940
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2023-02-15 23:26:23 +0700

    react main concept

--- ReactMainConcept/my-app/src/Form/UncontrollComponent.jsx
+++ ReactMainConcept/my-app/src/Form/UncontrollComponent.jsx
@@ -7,12 +7,27 @@ class UncontrollComponent extends Component {
 
     this.state = {};
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
     this.inputRef = createRef(); //dùng ref để truy cập vào DOM node để lấy dữ liệu của input
+    this.fileInputRef = createRef();
+    this.state = {
+      fileSelected: null
+    };
   }
 
   handleSubmit(event) {
     event.preventDefault();
     console.log(this.inputRef.current.value);
+
+    const formData = new FormData();
+
+    formData.append('myFile', this.state.fileSelected, 'bienBanGiaoNhan');
+  }
+
+  handleFileChange(event) {
+    this.setState({
+      fileSelected: event.target.files[0]
+    });
   }
 
   render() {
@@ -22,6 +37,15 @@ class UncontrollComponent extends Component {
           <div>
             <label>Input </label>
             <input type='text' name='name' ref={this.inputRef} />
+          </div>
+
+          <div>
+            <input
+              type='file'
+              name=''
+              ref={this.fileInputRef}
+              onChange={this.handleFileChange}
+            />
           </div>
 
           <button type='submit'>Submit</button>
