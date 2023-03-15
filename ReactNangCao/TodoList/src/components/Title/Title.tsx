86e0c2396b1e42bfbe28fad464ab3b9ba43e9bd7
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2023-03-15 23:14:53 +0700

    ref, useRef, React.forwardRef

--- ReactNangCao/TodoList/src/components/Title/Title.tsx
+++ ReactNangCao/TodoList/src/components/Title/Title.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ForwardedRef, useEffect, useRef, useState } from 'react';
 import styles from './title.module.scss';
 
 interface TitleProps {
@@ -11,9 +11,20 @@ interface TitleProps {
 function Title(props: TitleProps) {
   const { info } = props;
 
-  console.log('Title re-render');
+  const [color, setColor] = useState<string | undefined>();
 
-  return <h1 className={styles.title}>Todo list</h1>;
+  const h1Ref = useRef<HTMLHeadingElement | null>(null);
+
+  const changeColorH1 = () => {
+    if (!h1Ref.current) return;
+    //Cập nhật style trực tiếp bằng DOM thật
+    h1Ref.current.style.color = 'red';
+
+    //Cập nhật style thông qua DOM ảo
+    // setColor('red');
+  };
+
+  return <SubTitle ref={h1Ref} changeColorH1={changeColorH1} />;
 }
 
 // function equal(prevProps: TitleProps, nextProps: TitleProps) {
@@ -21,3 +32,21 @@ function Title(props: TitleProps) {
 // }
 
 export default React.memo(Title);
+
+interface SubTitleProps {
+  changeColorH1: () => void;
+}
+
+const SubTitle = React.forwardRef((props: SubTitleProps, ref: ForwardedRef<HTMLHeadingElement>) => {
+  const { changeColorH1 } = props;
+
+  useEffect(() => {
+    console.log(ref);
+  }, [ref]);
+
+  return (
+    <h1 className={styles.title} ref={ref} onClick={changeColorH1}>
+      Todo list
+    </h1>
+  );
+});
