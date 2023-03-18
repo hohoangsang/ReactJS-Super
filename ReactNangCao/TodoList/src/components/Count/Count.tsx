4669b9a41473cd0679ce4fac30eecdf82fe4d782
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2023-03-19 00:08:56 +0700

    done useLayoutEffect

--- ReactNangCao/TodoList/src/components/Count/Count.tsx
+++ ReactNangCao/TodoList/src/components/Count/Count.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 const handleHeavyTask = () => {
   const result = [];
@@ -12,11 +12,14 @@ const handleHeavyTask = () => {
 };
 
 export default function Count() {
-  const [count, setCount] = useState<number>(0);
+  // const [count, setCount] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
 
-  const handleIncrease = () => {
-    setCount((prev) => prev + 1);
-  };
+  const sectionRef = useRef<HTMLElement>(null);
+
+  // const handleIncrease = () => {
+  //   setCount((prev) => prev + 1);
+  // };
 
   // useEffect(() => {
   //   console.log('useEffect');
@@ -28,17 +31,39 @@ export default function Count() {
 
   // console.log('render');
 
-  useEffect(() => {
-    if (count === 4) {
-      handleHeavyTask();
-      setCount(0);
-    }
-  }, [count]);
+  // useLayoutEffect(() => {
+  //   if (count === 4) {
+  //     setCount(0);
+  //     alert(`Count: ${count}`);
+  //   }
+  // }, [count]);
+
+  useLayoutEffect(() => {
+    const handleSetWidth = () => {
+      setWidth(sectionRef.current?.offsetWidth || 0);
+    };
+
+    handleSetWidth();
+
+    window.addEventListener('resize', handleSetWidth);
+
+    return () => {
+      window.removeEventListener('resize', handleSetWidth);
+    };
+  }, []);
 
   return (
     <div>
-      <h3>Count: {count}</h3>
-      <button onClick={handleIncrease}>Increase Count</button>
+      {/* <h3>Count: {count}</h3>
+      <button onClick={handleIncrease}>Increase Count</button> */}
+
+      <section ref={sectionRef} style={{ backgroundColor: 'red' }}>
+        Main content
+      </section>
+
+      {width && width <= 400 ? (
+        <div style={{ backgroundColor: 'yellow' }}>Content show when width in less than 300px</div>
+      ) : null}
     </div>
   );
 }
